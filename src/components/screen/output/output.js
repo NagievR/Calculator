f45fe0736262096802b98e7cd123c5e0c7d9c48b
Ruslan Nagiev <ultraduck100@gmail.com>
2020-12-13 12:56:36 +0300

    just removed comment

--- src/components/screen/output/output.js
+++ src/components/screen/output/output.js
@@ -15,7 +15,6 @@ export function Output({ setSwitcherDisplaying }) {
       />
       <Result 
         displayWidth={displayWidth} 
-        // setSwitcherDisplaying={setSwitcherDisplaying} 
       />
     </div>
   );
