5c451b3f80fe3554cb37c6276710d951fcf511bf
Ruslan Nagiev <ultraduck100@gmail.com>
2020-12-14 15:49:34 +0300

    added text compression to the result

--- src/components/screen/output/result.js
+++ src/components/screen/output/result.js
@@ -3,20 +3,34 @@ import {React, useRef, useState, useEffect} from 'react';
 import './output.css';
 
 export function Result({ displayWidth }) {
-  const [isOversized, setIsOversized] = useState(false);
   const [number, setNumber] = useState('0');
+  const [fontSize, setFontSize] = useState(80);
   const resRef = useRef();
 
   useEffect(() => {
-    if (resRef.current.offsetWidth > displayWidth) {
-      setIsOversized(true);
+    if (resRef.current.offsetWidth >= displayWidth) {
+      setFontSize(prev => prev /= 1.18);
+    } 
+  }, [number, displayWidth]);
 
-    } // eslint-disable-next-line
-  }, [number]);
+  function addNumberTest() {
+    const maxNumberLength = 17;
+    if (number.length < maxNumberLength) {
+      return setNumber(prev => prev + Math.round(Math.random() * 10));
+    } else {
+      alert('Maximum number seize!');
+    }
+  }
 
   return (
-    <div className={`output-elem-wrap ${isOversized ? 'oversized' : ''}`}>
-      <span onClick ={() => setNumber(prev => prev + Math.round(Math.random() * 10))} ref={resRef} id='result'>{number}</span>
+    <div className={`output-elem-wrap`}> 
+      <span 
+        style={{fontSize: fontSize}}
+        onClick ={addNumberTest}
+        ref={resRef} 
+        id='result'>
+        {number}
+      </span>
     </div>
   );
 }
