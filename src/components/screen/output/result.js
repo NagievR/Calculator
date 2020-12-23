1bfee35af67a34fcfd72e21da190ae9feedb2eaf
Ruslan Nagiev <ultraduck100@gmail.com>
2020-12-24 02:54:25 +0300

    added default value 0

--- src/components/screen/output/result.js
+++ src/components/screen/output/result.js
@@ -1,23 +1,12 @@
-import { React, useRef, useState, useEffect } from 'react';
+import { React, useRef } from 'react';
 import { Textfit } from 'react-textfit';
 
 import './output.css';
 import { useStore } from "../../../logic/store.js";
 
 export function Result() {
-  const [number, setNumber] = useState('0');
   const { currentNumber } = useStore();
   const resRef = useRef();
-  
-  useEffect(() => {
-    console.log( String(Number.MAX_SAFE_INTEGER) );
-    const maxNumberLength = 21;
-    if (number.length < maxNumberLength) {
-      setNumber(currentNumber);
-    } else {
-      alert('Maximum number seize!'); 
-    }
-  }, [number, currentNumber]);
 
   return (
     <Textfit 
@@ -26,9 +15,17 @@ export function Result() {
       min={30}>
       <div className={`output-elem-wrap`}>   
         <span ref={resRef} id='result'>
-          {number}
+          {formateOutputValue(currentNumber) || '0'}
         </span>
       </div>
     </Textfit>
   );
 }
+
+// function formateOutputValue(n) {
+//   if (String(n).length > 3) {
+//     return n.slice(0, 1) + ' ' + n.slice(1);
+//   } else {
+//     return n
+//   }
+// }
