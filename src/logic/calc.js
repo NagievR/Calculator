e8d9cf024f112efe75dac57b7ccfdf109bf278a7
Ruslan Nagiev <ultraduck100@gmail.com>
2021-01-01 02:20:04 +0300

    added calculation functions

---
+++ src/logic/calc.js
@@ -0,0 +1,33 @@
+export function operatorsManager(currOp, operators, numbers) {
+  if (!operators.length) {
+    operators.push(currOp);
+    return;
+  }
+
+  const prevOp = operators[operators.length - 1];
+  if (prevOp.priority < currOp.priority) {
+    operators.push(currOp);
+  } else {
+    calculate(operators, numbers);
+    operators.push(currOp);
+  }
+
+  if (operators[1] && (operators[0].priority === operators[1].priority)) {
+    const lastOpSaved = operators.pop();
+    calculate(operators, numbers);
+    operators.push(lastOpSaved);
+  }
+};
+
+export function calculate(operators, numbers, iterations = 1) {
+  while (iterations) {
+    const [b, a] = [numbers.pop(), numbers.pop()];
+    const op = operators.pop();
+    const sum = op.doCalc(a, b);
+
+    numbers.push(sum);
+    console.log(`MEDIUM RESULT ${numbers[numbers.length - 1]}`);
+
+    iterations--;
+  }
+} 
