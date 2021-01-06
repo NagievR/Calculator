61b3c168d0e3db448d04142c9d1a5085f6be5c45
Ruslan Nagiev <ultraduck100@gmail.com>
2021-01-06 18:03:18 +0300

    minor code refactoring

--- src/components/screen/screen.js
+++ src/components/screen/screen.js
@@ -3,23 +3,9 @@ import { React, useState } from 'react';
 import './screen.css';
 import { JournalSwitcher } from './journal-switcher/journal-switcher.js';
 import { Output } from './output/output.js';
-// import { useDefineInputType } from "../../logic/define-input-type.js";
-
 
 export function Screen({ setJournalDisplaying }) {
   const [switcherDisplaying, setSwitcherDisplaying] = useState(false);
-
-  // useEffect(() => {
-  //   const timerId = setInterval(() => {
-  //     setSwitcherDisplaying(prev => !prev);
-  //     console.log(123)
-  //   }, 2000);
-
-  //   return () => clearInterval(timerId);
-  // });
-
-  // const c = useDefineInputType();
-  // console.log(c);
 
   return (
     <div id='screen'>
