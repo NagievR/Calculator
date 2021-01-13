3e221c56e6eab6ce27660b88a74aee261aac65d4
Ruslan Nagiev <ultraduck100@gmail.com>
2021-01-13 18:30:53 +0300

    providers moved from App to index.js. App should use context for kb handlers too

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ import React from 'react';
 import './index.css';
 import {App} from './app.js';
 
+// providers
+import { StoreProvider } from "./logic/store.js";
+import { HandlersCompositionProvider } from "./logic/handlers-composition.js";
+import { DefineInputTypeProvider } from "./logic/define-input-type.js";
+
 ReactDOM.render(
-  <App />,
+  <StoreProvider>
+    <HandlersCompositionProvider>
+      <DefineInputTypeProvider>
+        <App />
+      </DefineInputTypeProvider>
+    </HandlersCompositionProvider>
+  </StoreProvider>,
+
   document.getElementById('root')
 );
-
