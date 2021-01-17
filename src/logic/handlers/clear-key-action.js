271ff353e1eb2fff0eaf1d0392e80594b72b6aff
Ruslan Nagiev <ultraduck100@gmail.com>
2021-01-18 01:10:42 +0300

    impemented adding a log to history before clear

--- src/logic/handlers/clear-key-action.js
+++ src/logic/handlers/clear-key-action.js
@@ -6,11 +6,23 @@ export function clearKeyAction(store) {
     setHistory 
   } = store;
 
+  function addCalculatedPartOnly() {
+    const equalsIdx = log.lastIndexOf('=');
+    const calculatePart = log.slice(0, equalsIdx + 1);
+    const res = log[equalsIdx + 1];
+    setHistory(prev => prev.concat([[calculatePart.join(' '), res]]));
+  }
+
   return {
     clearKeyHandler() {
-      if (log.includes('=')) {
+      // add the log to history
+      const thereIsUncalculatedPart = log[log.length - 1] !== '=';
+      if (log.includes('=') && thereIsUncalculatedPart) {
+        addCalculatedPartOnly();
+      } else if (log.includes('=')) {
         setHistory(prev => prev.concat([[log.join(' '), result]]));
       }
+
       clearStore();
     },
     
